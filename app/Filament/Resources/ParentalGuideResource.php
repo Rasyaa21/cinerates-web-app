eb988d292639fa09956b72a8d8b7376dee3c4d61
Rasyaa21 <naufalrasya21907@gmail.com>
2024-11-13 20:37:24 +0700

    add filament to movie

--- app/Filament/Resources/ParentalGuideResource.php
+++ app/Filament/Resources/ParentalGuideResource.php
@@ -17,7 +17,7 @@ class ParentalGuideResource extends Resource
 {
     protected static ?string $model = ParentalGuide::class;
 
-    protected static ?string $navigationIcon = 'heroicon-o-tag';
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
 
     public static function form(Form $form): Form
     {
