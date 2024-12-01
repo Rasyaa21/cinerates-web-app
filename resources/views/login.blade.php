a0ab2bb932d5c8961c8e8d89a5aa63aeff379be0
Zharfan Zakkiyan <zharfanpintar@gmail.com>
2024-12-01 12:07:21 +0700

    top film jadi

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -7,31 +7,32 @@
     @vite('resources/css/app.css')
 </head>
 <body class="bg-bg-main">
-<section class="flex justify-center items-center pb-[100px] gap-[400px] mt-24">
-    <div class="sm:w-1/3 md:w-1/4 lg:w-1/6  flex justify-center mt-[100px]">
-        <div class="flex flex-col text-white pl-7">
-            <img src="\assets\img\poster-5.jpeg" class="max-w-full rounded-[16px] w-[2500px]">
+    <section class="flex justify-center my-24 mt-24" id="Home">
+        <div class="container flex flex-col items-center md:flex-row md:justify-between">
+            <div class="flex justify-center">
+                <img src="\assets\img\poster-5.jpeg" class=" rounded-[16px] max-w-full">
+            </div>
+            <div class="text-center justify-center md:text-justify md:pl-0 sm:items-center sm:mx-10">
+                <h1 class="mb-8 text-6xl font-bold text-white font-instrument-sans">CineRates</h1>
+                <h1 class="text-4xl text-white font-instrument-sans">Login To Cinerates</h1>
+                <p class="mt-3 text-xl text-white font-instrument-sans">Your Best Film Review</p>
+                <form class="">
+                    <div class="pt-10">
+                        <label for="Email" class="text-white font-instrument-sans" class="pb-10">Email </label> <br>
+                        <input type="text" name="Email" id="Email" class="px-10 py-2 rounded-xl">
+                    </div>
+                    <div class="pt-10">
+                        <label for="Password" class="text-white font-instrument-sans">Password</label> <br>
+                        <input type="password" name="password" id="password" class="px-10 py-2 rounded-xl">
+                    </div>
+                    <div class="mt-10">
+                        <button type="submit"  class="text-white bg-[#19ABE0] px-32 pt-[15px] pb-[15px] rounded-md text-2xl font-bold">Log In</button>
+                    </div>
+                </form>
+            </div>
+            </div>
         </div>
-    </div>
-    <div class="flex flex-col items-center justify-center mt-12 font-instrument-sans">
-        <h1 class="mb-8 text-6xl font-bold text-white font-instrument-sans">CineRates</h1>
-        <h1 class="text-4xl text-white font-instrument-sans">Login To Cinerates</h1>
-        <p class="mt-3 text-xl text-white font-instrument-sans">Your Best Film Review</p>
-        <form class="items-center justify-center">
-            <div class="pt-10">
-                <label for="Email" class="text-white font-instrument-sans">Email </label> <br>
-                <input type="text" name="Email" id="Email">
-            </div>
-            <div class="pt-10">
-                <label for="Password" class="text-white font-instrument-sans">Password</label> <br>
-                <input type="password" name="password" id="password">
-            </div>
-            <div class="items-center justify-center mt-10">
-                <button type="submit"  class="text-white bg-[#19ABE0] pl-[30px] pr-[30px] pt-[15px] pb-[15px] rounded-md">Log In</button>
-            </div>
-        </form>
-    </div>
-</section>
+    </section>
 </body>
 </html>
 </html>
---
+++ resources/views/topfilm.blade.php
@@ -0,0 +1,127 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Top Films</title>
+    @vite('resources/css/app.css')
+</head>
+<body class="bg-bg-main">
+    <nav id="nav">
+        <div class="p-5 lg:px-32 md:px-4 sm:px-3">
+            <div class="flex items-center justify-between">
+                <span class="text-3xl font-bold text-white-grayish font-poppins">CineRates</span>
+                <div class="md:hidden">
+                    <button id="menu-btn" class="text-white focus:outline-none">
+                        <svg class="w-6 h-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"/>
+                        </svg>
+                    </button>
+                </div>
+
+
+                <ul id="menu" class="absolute left-0 z-10 hidden w-full transition-all duration-100 ease-in-out transform bg-blue-800 opacity-0 md:flex md:items-center md:static md:bg-transparent md:w-auto top-20 md:top-0 md:opacity-100 md:translate-y-0">
+                    <li class="mx-4 my-6 md:my-0">
+                        <a href="#Home" class="text-xl font-semibold text-white duration-500 font-instrument-sans hover:text-blue-400">Home</a>
+                    </li>
+                    <li class="mx-4 my-6 md:my-0">
+                        <a href="#what-we-offer" class="text-xl font-semibold text-white duration-500 font-instrument-sans hover:text-blue-400">Feature</a>
+                    </li>
+                    <li class="mx-4 my-6 md:my-0">
+                        <a href="#trending" class="text-xl font-semibold text-white duration-500 font-instrument-sans hover:text-blue-400">Trending</a>
+                    </li>
+                    <li class="mx-4 my-6 md:my-0">
+                        <a href="#black-box-movies" class="text-xl font-semibold text-white duration-500 font-instrument-sans hover:text-blue-400">Box Office</a>
+                    </li>
+                    <li class="my-6 md:my-0">
+                        <button class="text-l bg-gradient-to-r from-[#1393ee] to-[#311fb0] mx-4 font-instrument-sans text-white font-semibold duration-500 px-6 py-2 hover:bg-cyan-500 rounded">
+                            Get Started
+                        </button>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <section class="flex flex-col items-center justify-center mt-40" id="trending">
+        <div class=" sm:w-2/3 md:w-1/3 lg:w-1/5 h-16 bg-gradient-to-r from-[#311FB0] to-[#2F80ED] rounded-[20px] flex items-center justify-center w-11/12">
+            <p class="text-xl font-semibold text-white font-instrument-sans">Trending Movies</p>
+        </div>
+
+
+        <div class="inline-grid lg:gap-20 md:gap-24 sm:gap-20 mt-10 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3">
+            <div class="flex flex-col">
+                <img src="\assets\img\tayang_tidalwave_fight_localized_742x1100_da1d8b60.jpeg" class="w-[150px] lg:w-[300px] md:w-[250px] rounded-[20px]">
+                <p class="font-instrument-sans text-xl font-bold text-white">Dedpol</p>               
+                <p class="font-instrument-sans text-white sm:text-sm md:text-xl">Mafia | Laga | Gangster</p>
+                <div class="flex">
+                    <p class="font-instrument-sans text-white lg:mr-[140px] md:mr-[100px] sm:w-[80px] sm:text-sm md:text-xl">2j 30m</p>
+                    <img src="\assets\img\bintang.png" class="md:w-[30px] sm:w-[20px]">
+                    <p class="font-instrument-sans text-xl font-bold text-white sm:text-sm md:text-xl">4.75/5</p>
+                </div>
+            </div>
+            <div class="flex flex-col">
+                <img src="\assets\img\tayang_tidalwave_fight_localized_742x1100_da1d8b60.jpeg" class="w-[150px] lg:w-[300px] md:w-[250px] rounded-[20px]">
+                <p class="font-instrument-sans text-xl font-bold text-white">Dedpol</p>               
+                <p class="font-instrument-sans text-white sm:text-sm md:text-xl">Mafia | Laga | Gangster</p>
+                <div class="flex">
+                    <p class="font-instrument-sans text-white lg:mr-[140px] md:mr-[100px] sm:w-[80px] sm:text-sm md:text-xl">2j 30m</p>
+                    <img src="\assets\img\bintang.png" class="md:w-[30px] sm:w-[20px]">
+                    <p class="font-instrument-sans text-xl font-bold text-white sm:text-sm md:text-xl">4.75/5</p>
+                </div>
+            </div>
+            <div class="flex flex-col">
+                <img src="\assets\img\tayang_tidalwave_fight_localized_742x1100_da1d8b60.jpeg" class="w-[150px] lg:w-[300px] md:w-[250px] rounded-[20px]">
+                <p class="font-instrument-sans text-xl font-bold text-white">Dedpol</p>               
+                <p class="font-instrument-sans text-white sm:text-sm md:text-xl">Mafia | Laga | Gangster</p>
+                <div class="flex">
+                    <p class="font-instrument-sans text-white lg:mr-[140px] md:mr-[100px] sm:w-[80px] sm:text-sm md:text-xl">2j 30m</p>
+                    <img src="\assets\img\bintang.png" class="md:w-[30px] sm:w-[20px]">
+                    <p class="font-instrument-sans text-xl font-bold text-white sm:text-sm md:text-xl">4.75/5</p>
+                </div>
+            </div>
+            <div class="flex flex-col">
+                <img src="\assets\img\tayang_tidalwave_fight_localized_742x1100_da1d8b60.jpeg" class="w-[150px] lg:w-[300px] md:w-[250px] rounded-[20px]">
+                <p class="font-instrument-sans text-xl font-bold text-white">Dedpol</p>               
+                <p class="font-instrument-sans text-white sm:text-sm md:text-xl">Mafia | Laga | Gangster</p>
+                <div class="flex">
+                    <p class="font-instrument-sans text-white lg:mr-[140px] md:mr-[100px] sm:w-[80px] sm:text-sm md:text-xl">2j 30m</p>
+                    <img src="\assets\img\bintang.png" class="md:w-[30px] sm:w-[20px]">
+                    <p class="font-instrument-sans text-xl font-bold text-white sm:text-sm md:text-xl">4.75/5</p>
+                </div>
+            </div>
+            <div class="flex flex-col">
+                <img src="\assets\img\tayang_tidalwave_fight_localized_742x1100_da1d8b60.jpeg" class="w-[150px] lg:w-[300px] md:w-[250px] rounded-[20px]">
+                <p class="font-instrument-sans text-xl font-bold text-white">Dedpol</p>               
+                <p class="font-instrument-sans text-white sm:text-sm md:text-xl">Mafia | Laga | Gangster</p>
+                <div class="flex">
+                    <p class="font-instrument-sans text-white lg:mr-[140px] md:mr-[100px] sm:w-[80px] sm:text-sm md:text-xl">2j 30m</p>
+                    <img src="\assets\img\bintang.png" class="md:w-[30px] sm:w-[20px]">
+                    <p class="font-instrument-sans text-xl font-bold text-white sm:text-sm md:text-xl">4.75/5</p>
+                </div>
+            </div>
+            <div class="flex flex-col">
+                <img src="\assets\img\tayang_tidalwave_fight_localized_742x1100_da1d8b60.jpeg" class="w-[150px] lg:w-[300px] md:w-[250px] rounded-[20px]">
+                <p class="font-instrument-sans text-xl font-bold text-white">Dedpol</p>               
+                <p class="font-instrument-sans text-white sm:text-sm md:text-xl">Mafia | Laga | Gangster</p>
+                <div class="flex">
+                    <p class="font-instrument-sans text-white lg:mr-[140px] md:mr-[100px] sm:w-[80px] sm:text-sm md:text-xl">2j 30m</p>
+                    <img src="\assets\img\bintang.png" class="md:w-[30px] sm:w-[20px]">
+                    <p class="font-instrument-sans text-xl font-bold text-white sm:text-sm md:text-xl">4.75/5</p>
+                </div>
+            </div>
+            <div class="flex flex-col">
+                <img src="\assets\img\tayang_tidalwave_fight_localized_742x1100_da1d8b60.jpeg" class="w-[150px] lg:w-[300px] md:w-[250px] rounded-[20px]">
+                <p class="font-instrument-sans text-xl font-bold text-white">Dedpol</p>               
+                <p class="font-instrument-sans text-white sm:text-sm md:text-xl">Mafia | Laga | Gangster</p>
+                <div class="flex">
+                    <p class="font-instrument-sans text-white lg:mr-[140px] md:mr-[100px] sm:w-[80px] sm:text-sm md:text-xl">2j 30m</p>
+                    <img src="\assets\img\bintang.png" class="md:w-[30px] sm:w-[20px]">
+                    <p class="font-instrument-sans text-xl font-bold text-white sm:text-sm md:text-xl">4.75/5</p>
+                </div>
+            </div>
+        </div>
+    </section>
+</body>
+</html>
